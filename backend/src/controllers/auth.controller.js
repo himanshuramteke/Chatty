98c5cd35c4f309dfea73497403898a3f67bacd0a
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-07-15 12:43:34 +0530

    Added mongodb connection and authRoutes

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signupController = (req, res) => {
+  res.send("signup route");
+};
+
+export const loginController = (req, res) => {
+  res.send("login route");
+};
+
+export const logoutController = (req, res) => {
+  res.send("signup route");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { PORT } from "./config/serverConfig.js";
+import authRoutes from "./routes/auth.routes.js";
+import { connectDB } from "./config/dbConfig.js";
 
 const app = express();
+
+app.use("/api/auth", authRoutes);
 
 app.get("/ping", (req, res) => {
   return res.json({ message: "Pong" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on PORT:${PORT}`);
+  connectDB();
 });
