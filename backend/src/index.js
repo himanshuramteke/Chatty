67cc76701c58cd80b9a5689c7921fd723dd1e58e
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-07-17 17:48:49 +0530

    Added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,15 +10,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
 app.use(
   cors({
-    origin: "http:localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.get("/ping", (req, res) => {
   return res.json({ message: "Pong" });
